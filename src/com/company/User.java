86a5600c1c8619388a401be92146dc9a31608f49
jtumms <john.tumminelli@tummsmedia.com>
2016-10-23 12:57:56 -0400

    Corrected mustache template issue on create-message route

--- src/com/company/User.java
+++ src/com/company/User.java
@@ -9,12 +9,38 @@ import java.util.ArrayList;
 public class User {
     String name;
     String password;
+    ArrayList<Message> messages = new ArrayList<>();
 
-    public User(String name, String password) {
+    public User(String name, String password, ArrayList<Message> messages) {
         this.name = name;
+        this.password = password;
+        this.messages = messages;
+    }
+
+    public User() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
         this.password = password;
     }
 
-    public User(String name) {
+    public ArrayList<Message> getMessages() {
+        return messages;
+    }
+
+    public void setMessages(ArrayList<Message> messages) {
+        this.messages = messages;
     }
 }
