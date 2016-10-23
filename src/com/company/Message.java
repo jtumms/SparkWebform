026b904f7b40f59e108563652042945c8bd5a4f6
jtumms <john.tumminelli@tummsmedia.com>
2016-10-23 12:29:57 -0400

    Updated to fix some issues

---
+++ src/com/company/Message.java
@@ -0,0 +1,22 @@
+package com.company;
+
+/**
+ * Created by john.tumminelli on 10/11/16.
+ */
+public class Message {
+
+    String text;
+
+    public Message(String text) {
+        this.text = text;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+}
+
